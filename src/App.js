d4b6fbd156a21780f26619e3b68e85a9d8538acb
Matt Hashimoto <matt@Matts-MacBook-Pro.local>
2022-04-30 09:36:38 -0700

    login w openLogin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import {} from 'dotenv/config'
 import { useState } from 'react';
+import OpenLogin from "@toruslabs/openlogin";
 import './App.css';
+import Login from './screens/Login';
+import Landing from './screens/Landing';
 
 function App() {
+  console.log(process.env)
   const [loggedIn, setLoggedIn] = useState(false);
-  return (
-    <div className="App">
-      <header className="App-header">
-        Magenta Finance.
-      </header>
-    </div>
-  );
+  const openlogin = new OpenLogin({ clientId: process.env.REACT_APP_OL_UID, network: "mainnet"});
+  if (loggedIn) {
+    return (
+      <Landing setLoggedIn={setLoggedIn} openlogin={openlogin} />
+    )
+  } else {
+    return (
+      <Login setLoggedIn={setLoggedIn} openlogin={openlogin} />
+    )
+  }
 }
 
 export default App;
